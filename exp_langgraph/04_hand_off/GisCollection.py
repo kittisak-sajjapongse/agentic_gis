1b473665607ade59fd1124d0648ea35aa9829258
Kittisak Sajjapongse <kittisak.sajjapongse@gmail.com>
2026-05-12 12:23:02 -0400

    Cleanup and delete legacy (unused) files

--- exp_langgraph/04_hand_off/GisCollection.py
+++
@@ -1,8 +0,0 @@
-"""Compatibility shim.
-
-Use domain.gis_catalog.GIS_COLLECTION for new imports.
-"""
-
-from domain.gis_catalog import GIS_COLLECTION
-
-__all__ = ["GIS_COLLECTION"]
--- exp_langgraph/04_hand_off/IAgentState.py
+++
@@ -1,8 +0,0 @@
-"""Compatibility shim.
-
-Use domain.state_models for new imports.
-"""
-
-from domain.state_models import GISFile, IAgentState
-
-__all__ = ["GISFile", "IAgentState"]
