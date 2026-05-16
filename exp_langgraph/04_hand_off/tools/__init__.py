def2ff6b0724a8bcea2b69370736f6a37c185c8b
Kittisak Sajjapongse <kittisak.sajjapongse@gmail.com>
2026-05-15 22:36:19 -0400

    Added ArtifactProvider as a one of the tools

--- exp_langgraph/04_hand_off/tools/__init__.py
+++ exp_langgraph/04_hand_off/tools/__init__.py
@@ -1,9 +1,13 @@
 from .base import ToolProvider
+from .artifact_provider import ArtifactMetadata, ArtifactProvider, LocalArtifactProvider
 from .gis_catalog_tools import GISCatalogToolProvider
 from .mcp_tools import DockerMCPToolProvider
 from .tool_executor import ToolExecutorNode
 
 __all__ = [
+    "ArtifactMetadata",
+    "ArtifactProvider",
+    "LocalArtifactProvider",
     "ToolProvider",
     "GISCatalogToolProvider",
     "DockerMCPToolProvider",
